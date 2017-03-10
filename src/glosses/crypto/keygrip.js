ab001872fbcb9345836dfedfe228b991f168f875
am <amedvedev@ciferox.com>
2017-03-10 19:14:34 +0200

    keygrip fixes * [crypto] keygrip fixes, added Keygrip.UrlSafe * [tests] crypto.keygrip

--- src/glosses/crypto/keygrip.js
+++ src/glosses/crypto/keygrip.js
@@ -1,32 +1,116 @@
 import adone from "adone";
+const { is, x, crypto: { stringCompare }, std: { crypto } } = adone;
 
-export default class {
-    constructor(keys = [], algorithm = "sha256", encoding = "base64") {
-        if (keys.length === 0) {
-            throw new adone.x.IllegalArgument("at least 1 key required");
+const cryptoHashes = new Set(crypto.getHashes());
+const cryptoCiphers = new Set(crypto.getCiphers());
+const hash = Symbol("hash");
+const cipher = Symbol("cipher");
+
+export default class Keygrip {
+    constructor(keys, { hash = "sha256", cipher = "aes-256-cbc" } = {}) {
+        if (!is.array(keys) || keys.length === 0) {
+            throw new x.InvalidArgument("Keys must be provided");
         }
         this.keys = keys;
-        this.algorithm = algorithm;
-        this.encoding = encoding;
+        this.hash = hash;
+        this.cipher = cipher;
     }
 
-    sign(data) {
-        return adone.std.crypto.createHmac(this.algorithm, this.keys[0])
-            .update(data)
-            .digest(this.encoding)
-            .replace(/\/|\+|=/g, (x) => ({ "/": "_", "+": "-", "=": "" })[x]);
+    get hash() {
+        return this[hash];
+    }
+
+    set hash(val) {
+        if (!cryptoHashes.has(val)) {
+            throw new x.NotSupported(`unsupported hash algorithm: ${val}`);
+        }
+        this[hash] = val;
+    }
+
+    get cipher() {
+        return this[cipher];
+    }
+
+    set cipher(val) {
+        if (!cryptoCiphers.has(val)) {
+            throw new x.NotSupported(`unsupported cipher: ${val}`);
+        }
+        this[cipher] = val;
+    }
+
+    encrypt(data, iv = null, key = this.keys[0]) {
+        const cipher = is.null(iv) ? crypto.createCipher(this.cipher, key) :
+                                     crypto.createCipheriv(this.cipher, key, iv);
+        const text = cipher.update(data, "utf8");
+        const pad = cipher.final();
+        return Buffer.concat([text, pad]);
+    }
+
+    decrypt(data, iv = null, key = null) {
+        if (is.null(key)) {
+            // check each key
+            const { keys } = this;
+            for (let i = 0; i < keys.length; ++i) {
+                const message = this.decrypt(data, iv, keys[i]);
+                if (message !== false) {
+                    return [message, i];
+                }
+            }
+            return false;
+        }
+        try {
+            const decipher = is.null(iv) ? crypto.createDecipher(this.cipher, key) :
+                                           crypto.createDecipheriv(this.cipher, key, iv);
+            const text = decipher.update(data, "utf8");
+            const pad = decipher.final();
+            return Buffer.concat([text, pad]);
+        } catch (err) {
+            return false;
+        }
+    }
+
+    sign(data, key = this.keys[0], encoding) {
+        return crypto.createHmac(this.hash, key).update(data).digest(encoding);
     }
 
     verify(data, digest) {
-        return this.index(data, digest) > -1;
+        return this.indexOf(data, digest) > - 1;
     }
 
-    index(data, digest) {
-        for (let i = 0; i < this.keys.length; ++i) {
-            if (adone.crypto.stringCompare(digest, this.sign(data, this.keys[i]))) {
+    indexOf(data, digest) {
+        const { keys } = this;
+        for (let i = 0; i < keys.length; ++i) {
+            if (stringCompare(digest, this.sign(data, keys[i]))) {
                 return i;
             }
         }
         return -1;
     }
 }
+
+const b64replaceRegExp = /\/|\+|=/g;
+
+const b64replaceFunc = (x) => {
+    switch (x) {
+        case "/": {
+            return "_";
+        }
+        case "+": {
+            return "-";
+        }
+        case "=": {
+            return "";
+        }
+        default: {
+            return x;
+        }
+    }
+};
+
+class UrlSafeKeygrip extends Keygrip {
+    sign(data, key) {
+        return super.sign(data, key, "base64").replace(b64replaceRegExp, b64replaceFunc);
+    }
+}
+
+Keygrip.UrlSafe = UrlSafeKeygrip;
---
+++ tests/glosses/crypto/keygrip.test.js
@@ -0,0 +1,134 @@
+describe("glosses", "crypto", "keygrip", () => {
+    const { crypto: { Keygrip }, std: { crypto } } = adone;
+
+    const testKeygripInstance = (keys, keylist) => {
+        let hash = keys.sign("hello world");
+
+        let index = keys.indexOf("hello world", hash);
+        assert.equal(index, 0);
+
+        const matched = keys.verify("hello world", hash);
+        assert.ok(matched);
+
+        index = keys.indexOf("hello world", "o_O");
+        assert.equal(index, -1);
+
+        // rotate a new key in, and an old key out
+        keylist.unshift("SEKRIT4");
+        keylist.pop();
+
+        // if index > 0, it's time to re-sign
+        index = keys.indexOf("hello world", hash);
+        assert.equal(index, 1);
+        hash = keys.sign("hello world");
+    };
+
+    describe("keygrip(keys)", () => {
+        it("should throw if keys are missing or empty", () => {
+            // keygrip takes an array of keys. If missing or empty, it will throw.
+            assert.throws(() => {
+                new Keygrip(/* empty list */);
+            }, /must be provided/);
+        });
+
+        it("should throw when setting an invalid hash algorithm", () => {
+            const keys = new Keygrip(["a", "b"]);
+            assert.throws(() => {
+                keys.hash = "asdf";
+            }, /unsupported/);
+        });
+
+        it("should throw when setting an invalid cipher", () => {
+            const keys = new Keygrip(["a", "b"]);
+            assert.throws(() => {
+                keys.cipher = "asdf";
+            }, /unsupported/);
+        });
+    });
+
+    describe("keygrip([key])", () => {
+        const keys = new Keygrip(["06ae66fdc6c2faf5a401b70e0bf885cb"]);
+
+        it("should sign a string", () => {
+            const hash = keys.sign("hello world");
+            assert.ok(/^[\w\+=]{44}$/.test(hash.toString("base64")));
+        });
+
+        it("should encrypt a message", () => {
+            const hash = keys.encrypt("lol");
+            assert.equal("lol", keys.decrypt(hash)[0].toString("utf8"));
+        });
+
+        it("should return false on bad decryptions", () => {
+            const keys2 = new Keygrip(["lkjasdf"]);
+            assert.equal(false, keys2.decrypt(keys.encrypt("lol")));
+        });
+
+        it("should return false on bad inputs", () => {
+            assert.equal(false, keys.decrypt(`${keys.encrypt("lol")}asdf`));
+        });
+    });
+
+    describe("keygrip([keys...])", () => {
+        it("should sign a string", () => {
+            const keylist = ["Newest", "AnotherKey", "Oldest"];
+            testKeygripInstance(new Keygrip(keylist), keylist);
+        });
+
+        it("should sign a string with a different algorithm and encoding", () => {
+            // now pass in a different hmac algorithm and encoding
+            const keylist = ["Newest", "AnotherKey", "Oldest"];
+            testKeygripInstance(new Keygrip(keylist), keylist);
+        });
+    });
+
+    describe("Message encryption", () => {
+        const length = 16;
+        const key = crypto.randomBytes(32);
+        const keygrip = new Keygrip([key]);
+
+        describe("with iv", () => {
+            const iv = crypto.randomBytes(length);
+            const message = keygrip.encrypt("lol, have Σπ", iv);
+
+            it("should encrypt and decrypt", () => {
+                assert.equal("lol, have Σπ", keygrip.decrypt(message, iv)[0].toString("utf8"));
+            });
+
+            it("should return false on invalid key", () => {
+                assert.equal(false, new Keygrip([crypto.randomBytes(32)])
+                    .decrypt(message, iv));
+            });
+
+            it("should return false on missing iv", () => {
+                assert.equal(false, keygrip.decrypt(message));
+            });
+
+            it("should return false on invalid iv", () => {
+                assert.equal(false, keygrip.decrypt(message, crypto.randomBytes(length)));
+            });
+        });
+
+        describe("without iv", () => {
+            const message = keygrip.encrypt("lol, have Σπ");
+
+            it("should encrypt and decrypt", () => {
+                assert.equal("lol, have Σπ", keygrip.decrypt(message)[0].toString("utf8"));
+            });
+
+            it("should return false on invalid key", () => {
+                assert.equal(false, new Keygrip([crypto.randomBytes(32)])
+                    .decrypt(message));
+            });
+
+            it("should work on really long strings", () => {
+                let string = "";
+                for (let i = 0; i < 10000; i++) {
+                    string += "a";
+                }
+                const msg = keygrip.encrypt(new Buffer(string));
+                assert.equal(string, keygrip.decrypt(msg)[0].toString("utf8"));
+            });
+        });
+    });
+});
