1d9f54e1e0c88bde491c37863d1612165f56a389
am <amedvedev@ciferox.com>
2017-04-03 19:42:51 +0200

    assertion * interfaces

--- src/glosses/assertion/lib/interface/expect.js
+++ src/glosses/assertion/lib/interface/expect.js
@@ -1,31 +1,12 @@
-/*!
- * chai
- * Copyright(c) 2011-2014 Jake Luer <jake@alogicalparadox.com>
- * MIT Licensed
- */
-
 export default function (lib) {
-    lib.expect = (val, message) => lib.getAssertion(val, message);
+    const { AssertionError, getAssertion } = lib;
 
-    /**
-     * ### .fail(actual, expected, [message], [operator])
-     *
-     * Throw a failure.
-     *
-     * @name fail
-     * @param {Mixed} actual
-     * @param {Mixed} expected
-     * @param {String} message
-     * @param {String} operator
-     * @namespace BDD
-     * @api public
-     */
+    lib.expect = (val, message) => getAssertion(val, message);
 
-    lib.expect.fail = function (actual, expected, message, operator) {
-        message = message || "expect.fail()";
-        throw new lib.AssertionError(message, {
+    lib.expect.fail = function (actual, expected, message = "expect.fail()", operator) {
+        throw new AssertionError(message, {
             actual,
-            expected, 
+            expected,
             operator
         }, lib.expect.fail);
     };
--- src/glosses/assertion/lib/interface/mock.js
+++ src/glosses/assertion/lib/interface/mock.js
@@ -1,15 +1,12 @@
+export default function mock(lib, utils) {
+    const { is, x } = adone;
+    const { Assertion } = lib;
 
+    const isSpy = (s) => is.function(s) &&
+        is.function(s.getCall) &&
+        is.function(s.calledWithExactly);
 
-const { is } = adone;
-
-export default function mock(chai, utils) {
-    function isSpy(putativeSpy) {
-        return is.function(putativeSpy) &&
-            is.function(putativeSpy.getCall) &&
-            is.function(putativeSpy.calledWithExactly);
-    }
-
-    function timesInWords(count) {
+    const timesInWords = (count) => {
         if (count === 1) {
             return "once";
         }
@@ -20,85 +17,95 @@ export default function mock(chai, utils) {
             return "thrice";
         }
         return `${count || 0} times`;
-    }
+    };
 
-    function isCall(putativeCall) {
-        return putativeCall && isSpy(putativeCall.proxy);
-    }
+    const isCall = (s) => s && isSpy(s.proxy);
 
-    function assertCanWorkWith(assertion) {
+    const assertCanWorkWith = (assertion) => {
         if (!isSpy(assertion._obj) && !isCall(assertion._obj)) {
-            throw new TypeError(`${utils.inspect(assertion._obj)} is not a spy or a call to a spy!`);
+            throw new x.InvalidArgument(`${utils.inspect(assertion._obj)} is not a spy or a call to a spy!`);
         }
-    }
+    };
 
-    function getMessages(spy, action, nonNegatedSuffix, always, args) {
+    const getMessages = (spy, action, nonNegatedSuffix, always, args) => {
         const verbPhrase = always ? "always have " : "have ";
         nonNegatedSuffix = nonNegatedSuffix || "";
         if (isSpy(spy.proxy)) {
             spy = spy.proxy;
         }
 
-        function printfArray(array) {
-            return spy.printf.apply(spy, array);
-        }
+        const printfArray = (array) => spy.printf.apply(spy, array);
 
         return {
             affirmative() {
-                return printfArray([`expected %n to ${verbPhrase}${action}${nonNegatedSuffix}`].concat(args));
+                return printfArray([
+                    `expected %n to ${verbPhrase}${action}${nonNegatedSuffix}`,
+                    ...args
+                ]);
             },
             negative() {
-                return printfArray([`expected %n to not ${verbPhrase}${action}`].concat(args));
+                return printfArray([
+                    `expected %n to not ${verbPhrase}${action}`,
+                    args
+                ]);
             }
         };
-    }
+    };
 
-    function mockProperty(name, action, nonNegatedSuffix) {
-        utils.addProperty(chai.Assertion.prototype, name, function () {
+    const mockProperty = (name, action, nonNegatedSuffix) => {
+        utils.addProperty(Assertion.prototype, name, function mockProperty() {
             assertCanWorkWith(this);
 
             const messages = getMessages(this._obj, action, nonNegatedSuffix, false);
             this.assert(this._obj[name], messages.affirmative, messages.negative);
         });
-    }
+    };
 
-    function mockPropertyAsBooleanMethod(name, action, nonNegatedSuffix) {
-        utils.addMethod(chai.Assertion.prototype, name, function (arg) {
+    const mockPropertyAsBooleanMethod = (name, action, nonNegatedSuffix) => {
+        utils.addMethod(Assertion.prototype, name, function asBoolean(arg) {
             assertCanWorkWith(this);
 
-            const messages = getMessages(this._obj, action, nonNegatedSuffix, false, [timesInWords(arg)]);
+            const messages = getMessages(
+                this._obj,
+                action,
+                nonNegatedSuffix,
+                false,
+                [timesInWords(arg)]
+            );
             this.assert(this._obj[name] === arg, messages.affirmative, messages.negative);
         });
-    }
+    };
 
-    function createMockMethodHandler(mockName, action, nonNegatedSuffix) {
-        return function (...args) {
-            assertCanWorkWith(this);
+    const createMockMethodHandler = (mockName, action, nonNegatedSuffix) => function (...args) {
+        assertCanWorkWith(this);
 
-            const alwaysMockMethod = `always${mockName[0].toUpperCase()}${mockName.substring(1)}`;
-            const shouldBeAlways = utils.flag(this, "always") && is.function(this._obj[alwaysMockMethod]);
-            const mockMethod = shouldBeAlways ? alwaysMockMethod : mockName;
+        const alwaysMockMethod = `always${mockName[0].toUpperCase()}${mockName.substring(1)}`;
+        const shouldBeAlways = utils.flag(this, "always") && is.function(this._obj[alwaysMockMethod]);
+        const mockMethod = shouldBeAlways ? alwaysMockMethod : mockName;
 
-            const messages = getMessages(this._obj, action, nonNegatedSuffix, shouldBeAlways, args);
-            this.assert(this._obj[mockMethod].apply(this._obj, args), messages.affirmative, messages.negative);
-        };
-    }
+        const messages = getMessages(this._obj, action, nonNegatedSuffix, shouldBeAlways, args);
+        this.assert(
+            this._obj[mockMethod](...args),
+            messages.affirmative,
+            messages.negative
+        );
+    };
 
-    function mockMethodAsProperty(name, action, nonNegatedSuffix) {
+    const mockMethodAsProperty = (name, action, nonNegatedSuffix) => {
         const handler = createMockMethodHandler(name, action, nonNegatedSuffix);
-        utils.addProperty(chai.Assertion.prototype, name, handler);
-    }
+        utils.addProperty(Assertion.prototype, name, handler);
+    };
 
-    function exceptionalMockMethod(chaiName, mockName, action, nonNegatedSuffix) {
+    const exceptionalMockMethod = (chaiName, mockName, action, nonNegatedSuffix) => {
         const handler = createMockMethodHandler(mockName, action, nonNegatedSuffix);
-        utils.addMethod(chai.Assertion.prototype, chaiName, handler);
-    }
+        utils.addMethod(Assertion.prototype, chaiName, handler);
+    };
 
-    function mockMethod(name, action, nonNegatedSuffix) {
+    const mockMethod = (name, action, nonNegatedSuffix) => {
         exceptionalMockMethod(name, name, action, nonNegatedSuffix);
-    }
+    };
 
-    utils.addProperty(chai.Assertion.prototype, "always", function () {
+    utils.addProperty(Assertion.prototype, "always", function always() {
         utils.flag(this, "always", true);
     });
 
