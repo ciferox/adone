832748995ac2ebe5364a4338dd1fe8b37f718d0d
am <amedvedev@ciferox.com>
2017-05-13 19:07:02 +0200

    examples * progress bar tick

---
+++ examples/terminal/progress/fast_ticks.js
@@ -0,0 +1,24 @@
+adone.run({
+    async main() {
+        const N = 5000;
+
+        const bar = adone.terminal.progress({
+            total: N,
+            schema: ":bar :spinner :current/:total (:elapsed)",
+            spinner: {
+                active: "bouncingBar"
+            },
+            timeFormatter: (d) => adone.datetime.utc(d).format("HH:mm:ss")
+        });
+
+        bar.tick(0);
+
+        for (let i = 0; i < N; ++i) {
+            bar.tick(1);
+            adone.info(`${i}) hello ${bar.current} ${bar.total}`);
+            await adone.promise.delay(20);
+        }
+
+        return 0;
+    }
+});
