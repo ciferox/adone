7620f30b3bdb09b700f7a1685c9e41cf3d707ebe
am <amedvedev@ciferox.com>
2017-05-18 13:22:20 +0200

    fix adone installing

--- src/cli/subsystems/meta/adone_manager.js
+++ src/cli/subsystems/meta/adone_manager.js
@@ -83,7 +83,7 @@ export default class AdoneManager {
 
     async installScript(name) {
         const scriptPath = this.getScriptPath(name);
-        const data = adone.templating.nunjucks.render(std.path.join(this.app.adoneEtcPath, "scripts", (is.win32 ? "adone.cmd" : "adone")), { targetPath: this.destAdoneDir.resolve("bin", "adone.js") });
+        const data = await adone.templating.nunjucks.render(std.path.join(this.app.adoneEtcPath, "scripts", (is.win32 ? "adone.cmd" : "adone")), { targetPath: this.destAdoneDir.resolve("bin", "adone.js") });
         if (await adone.fs.exists(scriptPath)) {
             await adone.fs.unlink(scriptPath);
         }
