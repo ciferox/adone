bd500e8ae82704fb39027cac3769d4b66dfba216
Maximus <smaxim@ciferox.com>
2017-05-26 23:23:28 +0200

    Add 'adone.net.ws.stream' namespace

--- src/glosses/net/ws/index.js
+++ src/glosses/net/ws/index.js
@@ -27,5 +27,6 @@ adone.lazify({
     constants: () => ({
         BINARY_TYPES: ["nodebuffer", "arraybuffer", "fragments"],
         GUID: "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
-    })
+    }),
+    stream: "./stream"
 }, exports, require);
