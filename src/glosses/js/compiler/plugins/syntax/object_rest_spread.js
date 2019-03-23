c29acbfde03b0bcd1105c3f9373ca92078f3773a
Maximus <smaxim@ciferox.com>
2019-03-24 01:05:55 +0300

    adone.js.compiler - Remove unused helpers. - Remove unused plugins.

--- src/glosses/js/compiler/plugins/syntax/object_rest_spread.js
+++
@@ -1,9 +0,0 @@
-export default adone.js.compiler.helper.pluginUtils.declare((api) => {
-    api.assertVersion(7);
-
-    return {
-        manipulateOptions(opts, parserOpts) {
-            parserOpts.plugins.push("objectRestSpread");
-        }
-    };
-});
